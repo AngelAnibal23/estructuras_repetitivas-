2dffa98f34ca9b9cb562827564802c699bee2cd9
Guts <aamamanica@unjbg.edu.pe>
2024-06-14 09:22:04 -0500

    Mejorando el texto de salida e inicializando la iteracion dentro del bucle for

--- Ejercicio1-repetitivas.cpp
+++ Ejercicio1-repetitivas.cpp
@@ -8,12 +8,12 @@ Diseñar un algoritmo que imprima en la pantalla los primeros “n” múltiplos de 7 
 using namespace std; 
 int main(){
 	
-	int	n, num=7, i; 
+	int	n, num=7; 
 	
 	cout<<"Digite la cantidad de multiplos que quiere obtener: "; cin>>n; 
-	for(i=1; i<=n; i=i+1){
+	for(int i=1; i<=n; i=i+1){
 		
-		cout<<"El primer numero es: "<<i<<" : "<<num*i<<endl; 
+		cout<<"El multiplo ->"<< i <<" es:"<<"  "<<num*i<<endl; 
 	}
 
 	
