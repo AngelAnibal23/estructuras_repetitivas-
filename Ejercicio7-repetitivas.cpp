5054210d440fe8d93210248e7aee80d49f65d3ec
Guts <aamamanica@unjbg.edu.pe>
2024-06-16 09:15:23 -0500

    Subiendo el ejercicio 7 de estructuras repetitivas

---
+++ Ejercicio7-repetitivas.cpp
@@ -0,0 +1,53 @@
+/*
+Ejercicio 7:
+Diseñar un algoritmo que permita leer “n” números por el teclado. El programa debe
+calcular el promedio de los números pares y el promedio de los números impares.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    
+         int n, numero, sp, si, i, cp, ci;
+         float pi, pp;
+         
+         i=0; sp=0;  si=0;  ci=0; cp=0;
+         
+        cout<<"Ingrese la cantidad de numeros que va a digitar: ";
+        cin>>n; 
+        
+        while(i<n){
+            cout<<"Ingrese un numero: ";
+            cin>>numero;
+            
+            if(numero%2==0){ 
+            sp=sp+numero;   
+            cp=cp+1;
+            }
+            else{
+            si=si+numero;
+            ci=ci+1;
+            }
+            i=i+1;
+        }
+        
+       if(ci>0){
+           pi=si/ci;
+           }
+       else{
+            pi=0;
+        }
+         if(cp>0){
+             pp=sp/cp;
+    	}  
+    	else{
+             pp=0; 
+        }
+              
+        cout<<"\nEl promedio de pares es: "<<pp<<endl;
+        cout<<"\nPromedio impares: "<<pi<<endl;
+              
+              
+       return 0; 
+ }
+
+
