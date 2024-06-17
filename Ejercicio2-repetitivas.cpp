9ada26c0469a771ffdf4a46eb189777738b058e5
Guts <aamamanica@unjbg.edu.pe>
2024-06-17 11:31:45 -0500

    Subiendo el ejercicio 2 de estructuras repetitivas

---
+++ Ejercicio2-repetitivas.cpp
@@ -0,0 +1,25 @@
+/*
+Ejercicio 2: 
+Leer numeros por el teclado hasta ingresar uno negativo.
+*/
+
+#include <iostream>
+using namespace std; 
+int main(){
+	
+	int numero;
+	char letra; 
+	numero=0; 
+	
+	while(numero>=0){
+			
+		cout<<"Ingrese cualquier numero: "; cin>>numero; 
+		
+		if(numero==letra){
+			numero=-1; 
+			cout<<"El dato ingresado no es valido"; 
+		}
+	}
+	
+	return 0; 
+}
